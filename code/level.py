99c9409c4b13efd3b30e0fabf0b063f6411cbe53
Edubedo <ezescobedo27@gmail.com>
2024-08-25 22:25:03 -0600

    Config of the movement object

--- code/level.py
+++ code/level.py
@@ -13,14 +13,29 @@ class Level:
         self.setup(tmx_map)  
 
     def setup(self, tmx_map):
+        # tiles
         # iterates over the tiles in the 'Terrain' layer of the Tiled map, creating a new Sprite instance for each tile at the correct position and adding it to the sprite groups and player
         for x, y, surf in tmx_map.get_layer_by_name('Terrain').tiles(): 
             Sprite((x * TILE_SIZE,y * TILE_SIZE), surf, (self.all_sprites, self.collision_sprites)) # Create a new sprite
  
+        # obj
         for obj in tmx_map.get_layer_by_name('Objects'): # Get the objects of the layer 'Objects'
             if obj.name == 'player': # If the object is the player
                 Player((obj.x, obj.y), self.all_sprites, self.collision_sprites)
-            
+        # mov objects
+        for obj in tmx_map.get_layer_by_name('Moving Objects'):
+            if obj.name == 'helicopter':
+                if obj.width > obj.height: # x
+                    move_dir = 'x'
+                    start_pos = (obj.x, obj.y + obj.height / 2)
+                    end_pos = (obj.x + obj.width, obj.y + obj.height / 2)
+                else: # y
+                    move_dir = 'y'
+                    start_pos = (obj.x + obj.width / 2, obj.y)
+                    end_pos = (obj.x + obj.width, obj.y + obj.height / 2)
+                speed = obj.properties['speed']
+                print(speed)
+
     def run(self, dt): # update all sprites
         self.display_surface.fill('black') # Change screen of the window
         self.all_sprites.update(dt)
--- code/sprites.py
+++ code/sprites.py
@@ -1,9 +1,14 @@
 from setting import *
 
 class Sprite(pygame.sprite.Sprite):
-    def __init__(self, pos, surf, groups):
+    def __init__(self, pos, surf = pygame.Surface((TILE_SIZE, TILE_SIZE)), groups = None):
         super().__init__(groups) # Call the __init__ method of the parent class
-        self.image = pygame.Surface((TILE_SIZE, TILE_SIZE)) # Create a new surface
+        self.image = surf # Create a new surface
         self.image.fill('white') # Fill the surface with white color
         self.rect = self.image.get_rect(topleft = pos) # Get the rectangle of the surface
         self.old_rect = self.rect.copy() # Get the rectangle of the surface
+
+class MovingSprite(Sprite):
+    def __init__ (self, groups, start_pos, end_pos, mode_dir, speed):
+        surf = pygame.Surface((200, 50))
+        super().__init__(start_pos, surf, groups)
