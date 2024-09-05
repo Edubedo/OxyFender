957b169373a70bb0b3282ef2b90022ab4c580abe
eescobedo <ezescobedo27@gmail.com>
2024-09-04 18:02:37 -0600

    Manejar copyright

--- menu/play/beginner/level1/level1.py
+++ menu/play/beginner/level1/level1.py
@@ -1,3 +1,5 @@
+# Código desarrollado por (E. Escobedo, G. Solorzano, R. Lavariga, N. Laureano, A. Suarez, S. Barroso) 2024
+# Este software no puede ser copiado o redistribuido sin permiso del autor.
 import pygame
 from utils.settings import *
 from os.path import join
