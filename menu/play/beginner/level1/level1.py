d58afefbc2ae999b3293dbf75b881c88c4419e23
eescobedo <ezescobedo27@gmail.com>
2024-09-11 09:06:53 -0600

    Hacer que el jugador se pueda mover por el mapa

--- menu/play/beginner/level1/level1.py
+++ menu/play/beginner/level1/level1.py
@@ -1,80 +1,54 @@
-# Código desarrollado por (E. Escobedo, G. Solorzano, R. Lavariga, N. Laureano, A. Suarez, S. Barroso) 2024
-# Este software no puede ser copiado o redistribuido sin permiso del autor.
 import pygame
 from utils.settings import *
 from os.path import join
-from pytmx.util_pygame import load_pygame # Importamos la libreria para cargar el mapa
+from pytmx.util_pygame import load_pygame
 import sys
 from menu.play.beginner.level1.sprites import Sprite
 from menu.play.beginner.level1.player import Player
-from random import uniform # Importamos la libreria para generar numeros aleatorios
+from random import uniform
 
 class Level1Beginner:
-    def __init__(self,tmx_map, name, dificultadNivel, id):
-        # Datos generales el nivel
+    def __init__(self, tmx_map, name, dificultadNivel, id):
         self.name = name
         self.dificultadNivel = dificultadNivel
         self.id = id
 
-        self.display_surface = pygame.display.get_surface() # Obtenemos la superficie de la pantalla
+        self.display_surface = pygame.display.get_surface()
 
-        # Groups
-        self.all_sprites = pygame.sprite.Group() # Creamos un grupo para todos los sprites(Objetos)
+        self.all_sprites = pygame.sprite.Group()
 
-        # Layer de abi
-        self.tmx_mapAbi = load_pygame(join("assets","maps","beginner","level1","SCIENCE.tmx"))
+        self.tmx_mapAbi = load_pygame(join("assets", "maps", "beginner", "level1", "SCIENCE.tmx"))
 
-        self.setup(self.tmx_mapAbi) # Llamamos al metodo setup para cargar los objetos del nivel
-        
-            
-            # Verificar si la capa es "Cableado" y tiene objetos
-            # if layer.name == "Cableado":
-            #     for obj in layer.objects:
-            #         print(f"Object: {obj.name}, Type: {obj.type}, X: {obj.x}, Y: {obj.y}, Width: {obj.width}, Height: {obj.height}")
+        self.camera_offset = pygame.Vector2(0, 0)
+        self.player = None
 
-            # for obj in layer.objects:
-            #     print(obj)
-       
-    # Establecemos el mapa principal
+        self.setup(self.tmx_mapAbi)
+
     def setup(self, tmx_map):
-        # Agregamos el mapa
         self.posicion_x_personaje = 0
-        # Agregamos el suelo del mapa
-        for x, y, surf in tmx_map.get_layer_by_name('Suelo').tiles(): 
-            Sprite((((x * TILE_SIZE) - self.posicion_x_personaje, y * TILE_SIZE)), surf, self.all_sprites, "RED") 
 
-        # Agregamos las paredes del mapa
+        for x, y, surf in tmx_map.get_layer_by_name('Suelo').tiles():
+            Sprite((((x * TILE_SIZE) - self.posicion_x_personaje, y * TILE_SIZE)), surf, self.all_sprites, "RED")
+
         for x, y, surf in tmx_map.get_layer_by_name('Paredes').tiles():
-            Sprite((((x * TILE_SIZE) - self.posicion_x_personaje, y * TILE_SIZE)), surf, self.all_sprites, "GREEN") 
+            Sprite((((x * TILE_SIZE) - self.posicion_x_personaje, y * TILE_SIZE)), surf, self.all_sprites, "GREEN")
 
-        # Agregamos el techo del mapa
-        for x, y, surf in tmx_map.get_layer_by_name('Techo').tiles(): 
-            Sprite((((x * TILE_SIZE) - self.posicion_x_personaje, y * TILE_SIZE)), surf, self.all_sprites, "ORANGE") 
+        for x, y, surf in tmx_map.get_layer_by_name('Techo').tiles():
+            Sprite((((x * TILE_SIZE) - self.posicion_x_personaje, y * TILE_SIZE)), surf, self.all_sprites, "ORANGE")
 
-        # Agregamos la pared del piso 1
-        for x, y, surf in tmx_map.get_layer_by_name('Techo').tiles(): 
-            Sprite((((x * TILE_SIZE) - self.posicion_x_personaje, y * TILE_SIZE)), surf, self.all_sprites, "ORANGE") 
+        for x, y, surf in tmx_map.get_layer_by_name('FondoPiso1').tiles():
+            Sprite((((x * TILE_SIZE) - self.posicion_x_personaje, y * TILE_SIZE)), surf, self.all_sprites, "WHITE")
 
-        # Agregamos el fondo del piso 1
-        for x, y, surf in tmx_map.get_layer_by_name('FondoPiso1').tiles(): 
-            Sprite((((x * TILE_SIZE) - self.posicion_x_personaje, y * TILE_SIZE)), surf, self.all_sprites, "WHITE") 
+        for x, y, surf in tmx_map.get_layer_by_name('FondoPiso2').tiles():
+            Sprite((((x * TILE_SIZE) - self.posicion_x_personaje, y * TILE_SIZE)), surf, self.all_sprites, "WHITE")
 
-        # Agregamos el fondo del piso 2
-        for x, y, surf in tmx_map.get_layer_by_name('FondoPiso2').tiles(): 
-            Sprite((((x * TILE_SIZE) - self.posicion_x_personaje, y * TILE_SIZE)), surf, self.all_sprites, "WHITE") 
-
-        # Agregamos el fondo del ascensor
-        for x, y , surf in tmx_map.get_layer_by_name("Ascensor").tiles():
+        for x, y, surf in tmx_map.get_layer_by_name("Ascensor").tiles():
             Sprite((((x * TILE_SIZE) - self.posicion_x_personaje, y * TILE_SIZE)), surf, self.all_sprites, "BLUE")
 
-        # Agregamos el jugador creando el Sprite Player
-        # for obj in tmx_map.get_layer_by_name('Objects'): # Obtenemos un objeto de tiled llamado objects
-        #     if obj.name == 'player': # Buscamos el objeto player en el mapa
-        #         Player((obj.x, obj.y), self.all_sprites)
+        self.player = Player((300, 300), self.all_sprites)
 
-        self.run() # Ejecutamos juego
+        self.run()
 
-    # Empezar a jugar     
     def run(self):
         clock = pygame.time.Clock()
 
@@ -85,29 +59,26 @@ class Level1Beginner:
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
-                # Mover el jugador
-                if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_LEFT:
-                        print("Izquierda")
-                        self.posicion_x_personaje -= 10
-                    if event.key == pygame.K_RIGHT:
-                        print("Derecha")
-                        self.posicion_x_personaje -= 10
-                    if event.key == pygame.K_UP:
-                        print("Arriba")
-                    if event.key == pygame.K_DOWN:
-                        print("Abajo")
-            # Actualizamos todos los sprite
+
+            keys = pygame.key.get_pressed()
+            if keys[pygame.K_LEFT]:
+                self.player.rect.x -= PLAYER_VEL
+            if keys[pygame.K_RIGHT]:
+                self.player.rect.x += PLAYER_VEL
+            if keys[pygame.K_UP]:
+                self.player.rect.y -= PLAYER_VEL
+            if keys[pygame.K_DOWN]:
+                self.player.rect.y += PLAYER_VEL
+
+            self.camera_offset.x = self.player.rect.centerx - self.display_surface.get_width() // 2
+            self.camera_offset.y = self.player.rect.centery - self.display_surface.get_height() // 2
+
             self.all_sprites.update()
 
-            # Rellenamos el fondo
-            self.display_surface.fill(BLACK)
-            self.all_sprites.draw(self.display_surface)
+            self.display_surface.fill(BACKGROUND_COLOR)
+            for sprite in self.all_sprites:
+                self.display_surface.blit(sprite.image, sprite.rect.topleft - self.camera_offset)
 
-            
-
-            #  Actualizamos los surface del juego
             pygame.display.flip()
 
-            # Establecemos los FPS del juego
-            clock.tick(60)
+            clock.tick(FPS)
