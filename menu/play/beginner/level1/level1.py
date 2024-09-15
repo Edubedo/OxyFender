3fe3fca521b6b260390dce3edfcd39868fca31f4
eescobedo <ezescobedo27@gmail.com>
2024-09-15 12:49:58 -0600

    avance

--- menu/play/beginner/level1/level1.py
+++ menu/play/beginner/level1/level1.py
@@ -43,7 +43,7 @@ class Level1Beginner:  # Creamos el nivel 1
                     self.colisiones_sprites.add(sprite)
 
         # Personaje
-        self.player = Player((418, 418), self.todos_los_sprites, 'RED')  # ! Establecer posicion del jugador de tiled
+        self.player = Player((420, 420), self.todos_los_sprites, 'RED')  # ! Establecer posicion del jugador de tiled
 
         self.run()
 
@@ -80,18 +80,26 @@ class Level1Beginner:  # Creamos el nivel 1
                     player_velocity_y = max_fall_speed
             player_movement.y += player_velocity_y
 
-            # Mover al jugador y verificar colisiones
-            self.player.rect.x += player_movement.x
-            if pygame.sprite.spritecollide(self.player, self.colisiones_sprites, False):
-                self.player.rect.x -= player_movement.x
-
+            # Mover al jugador y verificar colisiones verticales
             self.player.rect.y += player_movement.y
-            if pygame.sprite.spritecollide(self.player, self.colisiones_sprites, False):
-                self.player.rect.y -= player_movement.y
-                player_velocity_y = 0
-                on_ground = True
-            else:
-                on_ground = False
+            collided_sprites = pygame.sprite.spritecollide(self.player, self.colisiones_sprites, False)
+            for sprite in collided_sprites:
+                if player_movement.y > 0:  # Bajando
+                    self.player.rect.bottom = sprite.rect.top
+                    on_ground = True
+                    player_velocity_y = 0
+                elif player_movement.y < 0:  # Subiendo
+                    self.player.rect.top = sprite.rect.bottom
+                    player_velocity_y = 0
+
+            # Mover al jugador y verificar colisiones horizontales
+            self.player.rect.x += player_movement.x
+            collided_sprites = pygame.sprite.spritecollide(self.player, self.colisiones_sprites, False)
+            for sprite in collided_sprites:
+                if player_movement.x > 0:  # Moviéndose a la derecha
+                    self.player.rect.right = sprite.rect.left
+                elif player_movement.x < 0:  # Moviéndose a la izquierda
+                    self.player.rect.left = sprite.rect.right
 
             self.camera_offset.x = self.player.rect.centerx - self.mostrar_superficie.get_width() // 2
             self.camera_offset.y = self.player.rect.centery - self.mostrar_superficie.get_height() // 2
