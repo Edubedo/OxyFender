09fff586a028dd53cfc8bc0dcde4fcf45a4683b9
eescobedo <ezescobedo27@gmail.com>
2024-09-22 19:50:47 -0600

    Limpiar carpetas y cambiar algunas variables

--- menu/menu.py
+++ menu/menu.py
@@ -8,14 +8,13 @@ import sys
 
 class Menu:
     def __init__(self, screen):
-        self.screen = screen
-        pygame.display.set_caption(f"Menú - {TITLE_GAME}")
+        self.screen = screen 
+        pygame.display.set_caption(f"Menú - {TITLE_GAME}") # Establecemos tituloPrincipalJuego del Menú
 
         self.config = Configuration() # Establecemos la configuracion del lenguaje 
         self.font = pygame.font.Font(join("assets", "fonts", "Font_Menu_Options.ttf"), 18) # Establecemos la Fuente de texto
 
         # Música de fondo 
-
         # pygame.mixer.init() # Inicializar el módulo de sonido
         # pygame.mixer.music.load(join("assets", "audio", "music", "let_us_adore_you.mp3")) # Cargar la música
         # pygame.mixer.music.play(-1) # Reproducir la música en bucle
@@ -25,11 +24,11 @@ class Menu:
     def actualizarLenguajeTextos(self): # Actualizar el idioma de los textos
         language = self.config.obtenerLenguajeActual()
         if language == "english":
-            self.options = ["Play", "Credits", "Configuration", "Quit"]
+            self.opcionesMenuPrincipal = ["Play", "Credits", "Configuration", "Quit"]
         else:  
-            self.options = ["Jugar", "Créditos", "Configuración", "Salir"]
+            self.opcionesMenuPrincipal = ["Jugar", "Créditos", "Configuración", "Salir"]
 
-    def mostrarOpcionesMenu(self, indice_opcion_curso_encima=None): # Opciones del menú principal
+    def mostrarOpcionesMenu(self, hoverOpcionSeleccionada=None): # Opciones del menú principal
         self.background = pygame.image.load(join("assets", "img", "Background", "menu", "BackgroundProvisional2.jpg")).convert_alpha() # Agregar background al menú
         self.background = pygame.transform.scale(self.background, (WIDTH, HEIGHT)) # Escalar imagen del background del menú
 
@@ -37,27 +36,29 @@ class Menu:
         self.option_rects = []
 
         # Agregar el título del juego
-        fontTitulo = pygame.font.Font(join("assets", "fonts", "Triforce.ttf"), 100) # Establecemos la fuente del titulo
-        titulo = fontTitulo.render(TITLE_GAME, True, BLACK) #  Le agregamos color al titulo
-        titulo_rect = titulo.get_rect(center=(self.screen.get_width() // 2, 50)) # Creamos un rectangulo donde vamos a insertar el titulo del juego
-        self.screen.blit(titulo, titulo_rect) # Insertar el titulo de nuestro juego
+        fontTitulo = pygame.font.Font(join("assets", "fonts", "Triforce.ttf"), 100) # Establecemos la fuente del tituloPrincipalJuego
+        tituloPrincipalJuego = fontTitulo.render(TITLE_GAME, True, BLACK) #  Le agregamos color al tituloPrincipalJuego
+        recTituloPrincipalJuego = tituloPrincipalJuego.get_rect(center=(self.screen.get_width() // 2, 50)) # Creamos un rectangulo donde vamos a insertar el tituloPrincipalJuego del juego
+        self.screen.blit(tituloPrincipalJuego, recTituloPrincipalJuego) # Insertar el tituloPrincipalJuego de nuestro juego como imangen
 
-        for i, option in enumerate(self.options): # Mostrar las opciones del menú
-            if i == indice_opcion_curso_encima: # Si el cursor está encima de la opción
-                textOptionMenu = self.font.render(option, True, WHITE)
+        for i, opcionMenuPrincipal in enumerate(self.opcionesMenuPrincipal):
+            # Si el cursor está encima de alguna de las opciones principales pintar azul claro
+            if i == hoverOpcionSeleccionada: 
+                textoOpcionMenuPrincipal = self.font.render(opcionMenuPrincipal, True, WHITE)
                 background_rect = pygame.Surface((BUTTON_MENU_WIDTH, BUTTON_MENU_HEIGHT))
                 background_rect.fill(LIGHTBLUE)
+           # Si el cursor NO está encima de alguna de las opciones principales pintar azul oscuro
             else: # Si el cursor no está encima de la opción
-                textOptionMenu = self.font.render(option, True, WHITE)
+                textoOpcionMenuPrincipal = self.font.render(opcionMenuPrincipal, True, WHITE)
                 background_rect = pygame.Surface((BUTTON_MENU_WIDTH, BUTTON_MENU_HEIGHT))
                 background_rect.fill(DARK_BLUE)
             
-            text_rect = textOptionMenu.get_rect(topleft=(10, BUTTON_MENU_HEIGHT // 2 - textOptionMenu.get_height() // 2))
-            background_rect.blit(textOptionMenu, text_rect.topleft)
+            text_rect = textoOpcionMenuPrincipal.get_rect(topleft=(10, BUTTON_MENU_HEIGHT // 2 - textoOpcionMenuPrincipal.get_height() // 2))
+            background_rect.blit(textoOpcionMenuPrincipal, text_rect.topleft)
             
             rectOptionMenu = background_rect.get_rect(topleft=(0, 150 + i * 70))
             self.screen.blit(background_rect, rectOptionMenu)
-            self.option_rects.append((option.lower(), rectOptionMenu))
+            self.option_rects.append((opcionMenuPrincipal.lower(), rectOptionMenu))
         
         # Agregar texto en la esquina inferior derecha
         fontTextoInferiorDerecha = pygame.font.Font(join("assets", "fonts", "Font_Name_Enterprise.ttf"), 24)
@@ -68,19 +69,19 @@ class Menu:
         pygame.display.flip() # Actualizar pantalla
 
     def mostrarMenuInicial(self):
-        indice_opcion_curso_encima = None # Se usa para manejar el hover 
+        hoverOpcionSeleccionada = None # Se usa para manejar el hover 
 
         while True:
-            self.mostrarOpcionesMenu(indice_opcion_curso_encima)
+            self.mostrarOpcionesMenu(hoverOpcionSeleccionada)
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
                 elif event.type == pygame.MOUSEMOTION:
-                    indice_opcion_curso_encima = None
+                    hoverOpcionSeleccionada = None
                     for i, (_, rect) in enumerate(self.option_rects):
                         if rect.collidepoint(event.pos):
-                            indice_opcion_curso_encima = i
+                            hoverOpcionSeleccionada = i
                             pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_HAND)
                             break
                     else:
