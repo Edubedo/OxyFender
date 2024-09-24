5652f108977b4ba6d184e949f08643dda5119f41
eescobedo <ezescobedo27@gmail.com>
2024-09-23 20:21:47 -0600

    Copy

---
+++ utils/oxigenator.py
@@ -0,0 +1,2 @@
+# Código desarrollado por (E. Escobedo, G. Solorzano, R. Lavariga, N. Laureano, A. Suarez, S. Barroso) 2024
+# Este software no puede ser copiado o redistribuido sin permiso del autor.
--- utils/sprites.py
+++ utils/sprites.py
@@ -1,3 +1,5 @@
+# Código desarrollado por (E. Escobedo, G. Solorzano, R. Lavariga, N. Laureano, A. Suarez, S. Barroso) 2024
+# Este software no puede ser copiado o redistribuido sin permiso del autor.
 from utils.configuraciones import *
 
 class Sprite(pygame.sprite.Sprite):
