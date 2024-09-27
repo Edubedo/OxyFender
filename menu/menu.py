19b8f4a3d2ed16a061a5ec83f22ac5ffb3f1feb0
eescobedo <ezescobedo27@gmail.com>
2024-09-27 06:25:50 -0600

    Agregar diseños
    Diseño de portada

--- menu/menu.py
+++ menu/menu.py
@@ -34,35 +34,35 @@ class Menu:
         self.screen.blit(self.fondoPrincipalVideojuego, [0, 0]) # Acomodamos la imagenes del backgroud en la posición x=0 y=0
         self.rectOpcionesMenuPrincipal = []
 
-        # Agregar el título del juego
-        fontTitulo = pygame.font.Font(join("assets", "fonts", "ka1.ttf"), 100) # Establecemos la fuente del tituloPrincipalJuego
-        tituloPrincipalJuego = fontTitulo.render(TITLE_GAME, True, BLACK) #  Le agregamos color al tituloPrincipalJuego
-        recTituloPrincipalJuego = tituloPrincipalJuego.get_rect(center=(self.screen.get_width() // 2, 50)) # Creamos un rectangulo donde vamos a insertar el tituloPrincipalJuego del juego
-        self.screen.blit(tituloPrincipalJuego, recTituloPrincipalJuego) # Insertar el tituloPrincipalJuego de nuestro juego como imangen
+        # Agregar el título del juego como imagen
+        tituloPrincipalJuego = pygame.image.load(join("assets", "img", "TITULOS_FONDOS", "titulo_oxyfender.png")).convert_alpha()
+        tituloPrincipalJuego = pygame.transform.scale(tituloPrincipalJuego, (self.screen.get_width() // 2, 115)) # Escalar la imagen si es necesario
+        recTituloPrincipalJuego = tituloPrincipalJuego.get_rect(topleft=(-65, 5))
+        self.screen.blit(tituloPrincipalJuego, recTituloPrincipalJuego)
 
         for i, opcionMenuPrincipal in enumerate(self.opcionesMenuPrincipal): # Recorremos todas las opciones del menu principal
             
-            # Si el cursor está encima de alguna de las opciones pintar azul claro
+            # Si el cursor está encima de alguna de las opciones pintar azul claro y hacer el botón más largo
             if i == hoverOpcionSeleccionada: 
                 textoOpcionMenuPrincipal = self.font.render(opcionMenuPrincipal, True, WHITE)
-                fondoTextoOpcionMenuPrincipal = pygame.Surface((BUTTON_MENU_WIDTH + 10, BUTTON_MENU_HEIGHT))
+                fondoTextoOpcionMenuPrincipal = pygame.Surface((BUTTON_MENU_WIDTH + 40, BUTTON_MENU_HEIGHT + 10))
                 fondoTextoOpcionMenuPrincipal.fill(DARK_BLUE)
                 fondoTextoOpcionMenuPrincipal.set_alpha(255) # Full opacity
             # Si el cursor NO está encima de alguna de las opciones pintar azul oscuro
             else: # Si el cursor no está encima de la opción
                 textoOpcionMenuPrincipal = self.font.render(opcionMenuPrincipal, True, WHITE)
-                fondoTextoOpcionMenuPrincipal = pygame.Surface((BUTTON_MENU_WIDTH, BUTTON_MENU_HEIGHT))
+                fondoTextoOpcionMenuPrincipal = pygame.Surface((BUTTON_MENU_WIDTH + 10, BUTTON_MENU_HEIGHT + 10))
                 fondoTextoOpcionMenuPrincipal.fill(DARK_BLUE)
                 fondoTextoOpcionMenuPrincipal.set_alpha(204) # 0.8 opacity
             
             # A partir de cada opcion, dibujarle el rectangulo donde estárá centrada
-            rectTextoOpcionMenuPrincipal = textoOpcionMenuPrincipal.get_rect(topleft=(10, BUTTON_MENU_HEIGHT // 2 - textoOpcionMenuPrincipal.get_height() // 2))
+            rectTextoOpcionMenuPrincipal = textoOpcionMenuPrincipal.get_rect(topleft=(10, (BUTTON_MENU_HEIGHT + 10) // 2 - textoOpcionMenuPrincipal.get_height() // 2))
             
             # Pintar el texto dentro de cada fondo
             fondoTextoOpcionMenuPrincipal.blit(textoOpcionMenuPrincipal, rectTextoOpcionMenuPrincipal.topleft)
             
             # Posicionar los fondos 
-            rectOptionMenu = fondoTextoOpcionMenuPrincipal.get_rect(topleft=(0, 150 + i * 70))
+            rectOptionMenu = fondoTextoOpcionMenuPrincipal.get_rect(topleft=(0, 150 + i * 80)) # Más margen hacia abajo
             # Pintar los fondos dentro de la pantalla
             self.screen.blit(fondoTextoOpcionMenuPrincipal, rectOptionMenu)
             # Agregar las opciones a un arreglo para manejar el hover
